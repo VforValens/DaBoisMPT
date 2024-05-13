824e184b7a3626f340fad4df28fc86f4f872dce2
VforValens <8889280+VforValens@users.noreply.github.com>
2024-05-13 11:06:48 -0400

    Add new features
    Corrected line spacing for returnChancePercent. Add durabilityBurnRatio, overrideBuilTime, overrideCraftTime, and hideoutconfig dependency.

--- src/mod.ts
+++ src/mod.ts
@@ -7,14 +7,15 @@ import { BaseClasses } from "@spt-aki/models/enums/BaseClasses";
 import { ConfigServer } from "@spt-aki/servers/ConfigServer";
 import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
 import { IInsuranceConfig } from "@spt-aki/models/spt/config/IInsuranceConfig";
+import { IHideoutConfig } from "@spt-aki/models/spt/config/IHideoutConfig";
 
-class Bois implements IPostDBLoadMod {
+class DaBoisMPT implements IPostDBLoadMod {
     public postDBLoad(container: DependencyContainer): void {
-        // get database from server
+        // Get database from server
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         const configServer = container.resolve<ConfigServer>("ConfigServer");
         const insuranceConfig: IInsuranceConfig = configServer.getConfig<IInsuranceConfig>(ConfigTypes.INSURANCE);
-
+        const hideoutConfig: IHideoutConfig = configServer.getConfig<IHideoutConfig>(ConfigTypes.HIDEOUT);
 
         // Get all the in-memory json found in /assets/database
         const tables: IDatabaseTables = databaseServer.getTables();
@@ -25,10 +26,6 @@ class Bois implements IPostDBLoadMod {
 
         // Get globals settings and set flea market to sell non-FIR items.
         tables.globals.config.RagFair.isOnlyFoundInRaidAllowed = false;
-
-        // ---------------------------------------------------------
-        // Example #3
-        // Loop over all magazines and make them weigh nothing
 
         // Get ItemHelper ready to use
         const itemHelper: ItemHelper = container.resolve<ItemHelper>("ItemHelper");
@@ -43,9 +40,9 @@ class Bois implements IPostDBLoadMod {
         // We are filtering all items to only those with a base class of AMMO
         const ammos = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.AMMO));
 
-        // Loop over all the magazines the above code found
+        // Loop over all the ammo the above code found
         for (const ammo of ammos) {
-            // Check the magazine has a weight property before we edit it
+            // Check the ammo has a weight property before we edit it
             if (ammo._props.Weight) {
                 // Set its weight to 0
                 ammo._props.Weight = 0;
@@ -57,12 +54,15 @@ class Bois implements IPostDBLoadMod {
 
         // Loop over all the currency the above code found.
         for (const money of moneys) {
+            // Check the currency has a max stack size property before we edit it
             if (money._props.StackMaxSize) {
+                // Set the max stack size to 5,000,000
                 money._props.StackMaxSize = 5000000;
             }
         }
 
-        // Use tables
+        // Use the traders tables for Prapor and Therapist to set the max return hour,
+        // min return hour, and max storage time for insurance.
         tables.traders["54cb57776803fa99248b456e"].base.insurance.max_return_hour = 3;
         tables.traders["54cb57776803fa99248b456e"].base.insurance.max_storage_time = 720;
         tables.traders["54cb57776803fa99248b456e"].base.insurance.min_return_hour = 0
@@ -70,17 +70,40 @@ class Bois implements IPostDBLoadMod {
         tables.traders["54cb50c76803fa8b248b4571"].base.insurance.max_storage_time = 720;
         tables.traders["54cb50c76803fa8b248b4571"].base.insurance.min_return_hour = 0
 
-        // Use config
+        // Use the insurance config to set the insurance price multiplier for Therapist and Prapor
         insuranceConfig.insuranceMultiplier = {
             "54cb50c76803fa8b248b4571": 0.25,
             "54cb57776803fa99248b456e": 0.75
         }
 
+        // Use the insurance config to set the return chance percent for Therapist and Prapor
         insuranceConfig.returnChancePercent = {
             "54cb50c76803fa8b248b4571": 80,
-             "54cb57776803fa99248b456e": 100
+            "54cb57776803fa99248b456e": 100
         }
+
+        // Use the itemHelper class to assist us in getting only ammo
+        // We are filtering all items to only those with a base class of AMMO
+        const weapons = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.WEAPON));
+
+        // Loop over all the ammo the above code found
+        for (const weapon of weapons) {
+            // Check the ammo has a durability burn ratio property before we edit it
+            if (weapon._props.DurabilityBurnRatio) {
+                // Set its durability burn ratio to 0.75 or 75%
+                // Default is 1.15; 0.75 is a 35% reduction in burn ratio
+                weapon._props.DurabilityBurnRatio = 0.75;
+            }
+        }
+
+        // Set the overrideBuildTime in Seconds to 3600 (60x60 = 3600, 1 hour)
+        // So that building any single area in the hideout will take no more than 1 hour.
+        hideoutConfig.overrideBuildTimeSeconds = 3600;
+
+        // Set the overrideCraftTime in Seconds to 3600 (60x60 = 3600, 1 hour)
+        // So that crafting any single item in the hideout will take no more than 1 hour.
+        hideoutConfig.overrideCraftTimeSeconds = 3600;
     }
 }
 
-module.exports = { mod: new Bois() }
+module.exports = { mod: new DaBoisMPT() }
